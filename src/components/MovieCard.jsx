a9140a2ca3923b08094c6c72e95715326912cc64
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-10-05 14:36:38 -0300

    Movie card Component, and initial data

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
 //Will create a card for each movie to be shown on Movies Componente.
+import React from "react";
+
+class MovieCard extends React.Component{
+  render() {
+    const { movie: { title, subtitle, storyline, rating, imagePath, favorite } } = this.props;
+    return(
+      <div>
+        <h1>{ title }</h1>
+        <img src={ imagePath } alt={ title } />
+        <h2>{ subtitle }</h2>
+        <p>{ storyline }</p>
+        <div>
+          {favorite && <span role="star-icon" aria-label="Star Favorit">&#9733;</span>}
+          <h3>{ rating }</h3>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
