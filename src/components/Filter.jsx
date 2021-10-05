31ad9efd396c05e282ea3c03aa99a0d5b430e026
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-10-05 15:39:07 -0300

    Creat Static Filter Component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,1 +1,33 @@
 //Will filer the data and create a new array of objects to put on Content state to be show on movies component.
+import React from 'react';
+import movies from '../data';
+
+class Filter extends React.Component{
+  render() {
+    return(
+      <div>
+        <form>
+          <label>
+            Contem o texto:
+            <input type="text" />
+          </label>
+          <label>
+            Mostrar apenas favoritos
+            <input type="checkbox" />
+          </label>
+          <label>
+            Genero:
+            <select>
+              <option>Action</option>
+              <option>Comedy</option>
+              <option>Romance</option>
+              <option>New Releases</option>
+            </select>
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Filter;
