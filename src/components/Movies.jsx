6973e96ca1c5280a2dc0d4b53761b89cfa954c15
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-10-05 14:48:11 -0300

    Movies component

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,1 +1,29 @@
 //will display the filtered data from datausing the MovieCard componente.
+import React from "react";
+import MovieCard from "./MovieCard";
+
+class Movies extends React.Component{
+  constructor(){
+    super();
+
+    this.state = {
+        loading: false,
+        movies: [],
+    }
+  }
+
+  render() {
+    const { movies, loading } = this.state;
+    return(
+      <div>
+        <h1>Movies</h1>
+        {loading && <>Loading...</>}
+        <div>
+          {movies && movies.map((movie, index) => <MovieCard key={index} movie={ movie } />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Movies;
